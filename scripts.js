3d4eca43fe177abe4c756750a05cf75cf727795e
Aditya Bose <13054902+adbose@users.noreply.github.com>
2020-08-29 21:20:47 +0530

    Refactor code

--- scripts.js
+++ scripts.js
@@ -5,3 +5,8 @@ function getTime() {
     document.getElementById('clock').innerHTML = x;
     setTimeout(getTime, 1000);
 }
+
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
